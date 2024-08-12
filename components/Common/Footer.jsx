61535f6e2ab7e65827067c732a2a9a6dbfef65f7
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-08-12 17:07:11 +0900

    feat : 푸터 컴포넌트 생성

---
+++ components/Common/Footer.jsx
@@ -0,0 +1,38 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const footerStyle = css`
+  background-color: transparent; /* 배경 투명 */
+  border-top: 2px solid brown; /* 맨 위 갈색 선 */
+  color: #000;
+  text-align: left;
+  padding: 20px 0;
+  position: fixed;
+  bottom: 0;
+
+  width: 100%;
+  height: 13   0px;
+  
+`;
+const strongTextStyle = css`
+  font-size: 14px;
+  font-weight: bold; 
+  margin: 20px 0px 0px 50px;
+`;
+
+const normalTextStyle = css`
+  font-size: 14px;
+  margin: 10px 0px 0px 50px;
+`;
+
+
+const Footer = () => {
+  return (
+    <footer css={footerStyle}>
+      <p css={strongTextStyle}>카카오 부트캠프 2조: 춘식2</p>
+      <p css={normalTextStyle}>박세호 박지혁 안태진 정은채 한주리</p>
+    </footer>
+  );
+};
+
+export default Footer;
