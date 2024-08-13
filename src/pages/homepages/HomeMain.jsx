d44994cd92be4a03baf606ac9b821591c9c6f8ad
sayyyho <323psh@naver.com>
2024-08-13 16:03:16 +0900

    Test: CI/CD 최종 테스트

--- src/pages/homepages/HomeMain.jsx
+++ src/pages/homepages/HomeMain.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import * as main from './HomeMain_styled';
-import Header from '../../components/Header';
+import React from "react";
+import * as main from "./HomeMain_styled";
+import Header from "../../components/Header";
 
 function HomeMain() {
-    return (
-        <main.Container>
-            <main.Header>
-                <Header/>
-            </main.Header>
-            <main.Main>
-            </main.Main>
-        </main.Container>
-    )
-  }
-  
-  export default HomeMain;
-  
+  return (
+    <main.Container>
+      <main.Header>
+        <Header />
+      </main.Header>
+      <main.Main>CI/CD 테스트!</main.Main>
+    </main.Container>
+  );
+}
+
+export default HomeMain;
