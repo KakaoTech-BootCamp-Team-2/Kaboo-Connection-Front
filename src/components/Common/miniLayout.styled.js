39225a5cdd7520975a3f0cf40c2652e1e3501396
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-08-16 16:23:44 +0900

    minilayout 컴포넌트 만들

---
+++ src/components/Common/miniLayout.styled.js
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+
+export const LayoutContainer = styled.div`
+  width: ${({ width }) => width || "100%"};
+  box-sizing: border-box;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 3px;
+  background-color: #715f00;
+  margin: 15px 0px 15px 0px;
+`;
+
+export const CenterContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px;
+  margin: 10px;
+`;
