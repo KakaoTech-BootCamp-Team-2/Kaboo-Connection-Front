ecb7ed4f13d13bb31a4a454cba8c3cc9f7e913b2
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-08-24 23:43:27 +0900

    fix:주스터드 오류 수정

--- src/stores/MarkDownStore.js
+++ src/stores/MarkDownStore.js
@@ -1,10 +1,10 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-const MarkDownStore = create(set => ({
+const useMarkDownStore = create(set => ({
     content: '### **자기소개** 부탁드려요! \n 블로그, 깃허브 링크 첨부 등 마크다운 문법을 이용하여 자유롭게!',
     editMode: false,
-    setContent: (newContent) => set({ content: newContent }),
-    setEditMode: (newEditMode) => set({ editMode: newEditMode })
+    setContent: newContent => set({ content: newContent }),
+    setEditMode: newEditMode => set({ editMode: newEditMode })
 }));
 
-export default MarkDownStore;
+export default useMarkDownStore;
