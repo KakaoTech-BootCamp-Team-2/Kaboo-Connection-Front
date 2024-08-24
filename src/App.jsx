a91346ebadc77530b54f63d740ce28cf370ebba9
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-08-24 22:45:48 +0900

    페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import GlobalStyle from "./components/Common/GlobalStyle";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { PeopleListPage } from "./pages/PeopleListPage/PeopleListPage.jsx";
 import { PeopleDetailPage } from "./pages/PeopleDetailPage/PeopleDetailPage.jsx";
+
+
 function App() {
   return (
     <>
       <GlobalStyle />
-      <PeopleDetailPage></PeopleDetailPage>
+        <Router>
+            <Routes>
+                <Route path="/" element={<PeopleListPage />} />
+                <Route path="/details" element={<PeopleDetailPage />} />
+            </Routes>
+        </Router>
     </>
   );
 }
