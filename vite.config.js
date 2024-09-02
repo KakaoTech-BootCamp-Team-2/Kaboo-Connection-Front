d605f17cda2e337a37e8b62081784503e8b30c06
sayyyho <323psh@naver.com>
2024-09-02 14:32:49 +0900

    Fix: API 엔드포인트 에러 수정

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      // 모든 요청을 프록시할 수 있도록 설정
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // '/api' 부분을 제거
-        secure: false,
-        ws:true,
-      },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
     },
-  },
-  define: {
-    global: 'window', // 글로벌 변수를 브라우저 환경에서 사용할 수 있도록 설정
   },
 });
