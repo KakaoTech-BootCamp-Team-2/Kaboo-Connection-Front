5b2640816d9159f0b682c3d6f6b40a4d3aeaa39a
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-09-10 16:05:20 +0900

    충돌 해소

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
+import { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link,
+  useLocation,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import HomeMain from "./pages/homepages/HomeMain";
 import { PeopleListPage } from "./pages/PeopleListPage/PeopleListPage.jsx";
 import { PeopleDetailPage } from "./pages/PeopleDetailPage/PeopleDetailPage.jsx";
 import ChatPage from "./pages/chat/ChatPage";
-import KakaoRedirect from "./pages/join/KakoRedirect.jsx";
-import JoinPage from "./pages/join/JoinPage.jsx";
 import RTCPage from "./pages/WebRTC/VideoPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { PeopleListPage } from './pages/PeopleListPage/PeopleListPage.jsx';
-import { PeopleDetailPage } from './pages/PeopleDetailPage/PeopleDetailPage.jsx';
-import ChatPage from './pages/chat/ChatPage';
-import RTCPage from './pages/VideoPage';
-import LoginPage from "./pages/kakaologin/LoginPage.jsx";
-// QueryClient 인스턴스 생성
+
+// QueryClient 인스턴스 생성ß
 const queryClient = new QueryClient();
 
 function App() {
@@ -26,17 +21,14 @@ function App() {
       <QueryClientProvider client={queryClient}>
     <Router>
       <Routes>
-        <Route path="/" element={<PeopleListPage />}></Route>
+        <Route path="/" element={<HomeMain />}></Route>
         <Route path="/chat" element={<ChatPage />}></Route>
-        <Route path="/video/:roomName" element={<RTCPage />}></Route>
+        <Route path="/video" element={<RTCPage />}></Route>
         <Route path="/list" element={<PeopleListPage />} />
         <Route path="/details" element={<PeopleDetailPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/auth/code/kakao" element={<KakaoRedirect />} />
-        <Route path="/join" element={<JoinPage />} />
       </Routes>
     </Router>
-       </QueryClientProvider>
+      </QueryClientProvider>
   );
 }
 
