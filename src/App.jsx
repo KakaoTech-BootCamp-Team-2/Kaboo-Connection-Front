3423e9bdb292717ded38ef04a0130ecb71f9a03b
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-09-10 16:57:34 +0900

    fix:로그인다시 연결

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,27 @@ import { PeopleListPage } from "./pages/PeopleListPage/PeopleListPage.jsx";
 import { PeopleDetailPage } from "./pages/PeopleDetailPage/PeopleDetailPage.jsx";
 import ChatPage from "./pages/chat/ChatPage";
 import RTCPage from "./pages/WebRTC/VideoPage.jsx";
+import LoginPage from "./pages/kakaologin/LoginPage.jsx";
+import KakaoRedirect from "./pages/join/KakoRedirect.jsx";
+import JoinPage from "./pages/join/JoinPage.jsx";
 
 // QueryClient 인스턴스 생성ß
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomeMain />}></Route>
-          <Route path="/chat" element={<ChatPage />}></Route>
-          <Route path="/video/:roomName" element={<RTCPage />}></Route>
-          <Route path="/list" element={<PeopleListPage />} />
-          <Route path="/details" element={<PeopleDetailPage />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PeopleListPage />}></Route>
+        <Route path="/chat" element={<ChatPage />}></Route>
+        <Route path="/video/:roomName" element={<RTCPage />}></Route>
+        <Route path="/list" element={<PeopleListPage />} />
+        <Route path="/details" element={<PeopleDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/auth/code/kakao" element={<KakaoRedirect />} />
+        <Route path="/join" element={<JoinPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -19,14 +19,17 @@ function Header() {
         </styles.LogoWrapper>
 
         <styles.NavBar>
-          <styles.NavItem href="/">카부 게시판</styles.NavItem>
-          <styles.NavItem href="/login">카부 프로젝트</styles.NavItem>
-          <styles.NavItem href="/">카부 편의</styles.NavItem>
+          <styles.NavItem href="/list">카부 게시판</styles.NavItem>
+          <styles.NavItem href="/list">카부 프로젝트</styles.NavItem>
+          <styles.NavItem href="/list">카부 편의</styles.NavItem>
           <styles.NavItem href="/chat">카부 커넥션</styles.NavItem>
         </styles.NavBar>
       </styles.HeaderSubContainer>
       <styles.PeopleLogoWrapper>
-        <styles.PeopleLogo src={peoplelogo} />
+        <styles.PeopleLogo
+            onClick={() => {
+            navigate("/login");
+        }} src={peoplelogo} />
       </styles.PeopleLogoWrapper>
     </styles.HeaderContainer>
   );
